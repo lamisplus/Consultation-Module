da095e86e82712a6cd2e1b8732ad6dc33ed786ba
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-07-21 11:14:38 +0100

    fix: updated some configs

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,7 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-export const apiUrl = "/api/v1/";
-// const devToken = process.env.REACT_APP_API_TOKEN;
-
-// export const url = "http://localhost:8383/api/v1/";
-// export const apiUrl = "http://localhost:8383/api/";
-// export const token = devToken;
+// export const token = new URLSearchParams(window.location.search).get('jwt');
+// export const url = '/api/v1/';
+// export const apiUrl = '/api/v1/';
+const devToken = process.env.REACT_APP_API_TOKEN;
+export const url = 'http://localhost:8383/api/v1/';
+export const apiUrl = 'http://localhost:8383/api/';
+export const token = devToken;

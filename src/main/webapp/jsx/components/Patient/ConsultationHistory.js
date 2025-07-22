25902858cf93ba4b19b614f3d17424767d2fcefe
gamalieldashuaDataFi <gamaliel.dashua@thepalladiumgroup.com>
2025-07-22 09:37:27 +0100

    Update ConsultationHistory.js

--- src/main/webapp/jsx/components/Patient/ConsultationHistory.js
+++ src/main/webapp/jsx/components/Patient/ConsultationHistory.js
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from "react";
-import MaterialTable from "@material-table/core";
-import axios from "axios";
-import { url as baseUrl, token } from "./../../../api";
+import React, { useEffect, useState } from 'react';
+import MaterialTable from '@material-table/core';
+import axios from 'axios';
+import { url as baseUrl, token } from './../../../api';
 
-import { forwardRef } from "react";
-import "semantic-ui-css/semantic.min.css";
-import { Link } from "react-router-dom";
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowUpward from "@material-ui/icons/ArrowUpward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
-import { Card, CardBody } from "reactstrap";
+import { forwardRef } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { Link } from 'react-router-dom';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import { Card, CardBody } from 'reactstrap';
+import PatientDashboard from './PatientDashboard';
 
 const tableIcons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
@@ -51,7 +52,7 @@ function ConsultationHistory() {
   return (
     <div>
       <Card>
-        <CardBody>
+        {/* <CardBody>
           <MaterialTable
             icons={tableIcons}
             title="Find Patient "
@@ -92,7 +93,8 @@ function ConsultationHistory() {
               sorting: false, // Disable user sorting to maintain queue order
             }}
           />
-        </CardBody>
+        </CardBody> */}
+        <PatientDashboard />
       </Card>
     </div>
   );

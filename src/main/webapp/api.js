7721eb1015b3ca4049b8196b81ab67b3c864bfd5
Emmanuel Chinonye Nnajiofor <emmanuel.nnajiofor@thepalladiumgroup.com>
2025-11-24 09:11:58 +0100

    minor update

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -10,7 +10,7 @@ export const apiUrl =
 
 export const token =
     process.env.NODE_ENV === "development"
-        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZGUtdXNlciIsImF1dGgiOiJTdXBlciBBZG1pbixVc2VyLFJERSIsIm5hbWUiOiJyZGUtdXNlciBsYXN0bmFtZSIsImV4cCI6MTc2MjQ0NzYxMX0.O1GUWR4UM6CHMQFE2p83gGvPqqnv4SiAL3J-Q6pJiYlDRlIG_6nOIVnVx6bSWyafqMWrTY0wv3_CZ7MIq4_JKQ"
+        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZGUtdXNlciIsImF1dGgiOiJTdXBlciBBZG1pbixVc2VyLFJERSIsIm5hbWUiOiJyZGUtdXNlciBsYXN0bmFtZSIsImV4cCI6MTc2MjU2NDIxMn0.m1SNN6HZIF3FUXSmxep-RKDCT2savTvYmEN2yk4mlC9gKDK179oEeoUJKUw0T0an3dm2OtUV2Gp0GyZWPSZiDg"
         : new URLSearchParams(window.location.search).get("jwt");
 
 export const audioTranscriptionUrl = process.env.NODE_ENV === "development"
